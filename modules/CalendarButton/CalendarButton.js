c4a8c14cc27dd26b62ebb18c2381eb73ed2c0ce2
mmelezanovic <maisa.melezanovic@hotmail.com>
2020-10-15 18:15:53 +0200

    add calendarbutton moduke

---
+++ modules/CalendarButton/CalendarButton.js
@@ -0,0 +1,29 @@
+Module.register("CalendarButton", {
+	// Default module config.
+	defaults: {},
+
+	start: function () {
+		Log.log("begin start function!");
+	},
+
+	// Override dom generator.
+	getDom: function () {
+		var wrapper = document.createElement("div");
+		var button = document.createElement("a");
+		button.style.width = "300px";
+		button.style.height = "100px";
+		button.style.display = "block";
+		button.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+		//button.style.Position = "fixed",
+		//button.style.Top = "2px",
+		//button.style.Left = "2px",
+		//button.style.Z - index = "4",
+		//var self = this;
+		button.onclick = () => {
+			console.log("--------------Clicked");
+			this.sendNotification("CAROUSEL_MODULE_TRANSITION_CALENDAR", {});
+		};
+		wrapper.appendChild(button);
+		return wrapper;
+	}
+});
